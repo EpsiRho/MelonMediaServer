7f16c1546d768df2cf3be1901089e74d52a16ad7
Epsi <epsirho@gmail.com>
2024-02-21 22:07:36 -0600

    fix plugin load

--- Code/Melon/LocalClasses/StateManager.cs
+++ Code/Melon/LocalClasses/StateManager.cs
@@ -359,19 +359,10 @@ namespace Melon.LocalClasses
                 }
             }
         }
-        private static Assembly LoadPlugin(string relativePath)
+        private static Assembly LoadPlugin(string path)
         {
-            // Navigate up to the solution root
-            string root = Path.GetFullPath(Path.Combine(
-                Path.GetDirectoryName(
-                    Path.GetDirectoryName(
-                        Path.GetDirectoryName(
-                            Path.GetDirectoryName(
-                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
-
-            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
-            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
-            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+            PluginLoadContext loadContext = new PluginLoadContext(path);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(path)));
         }
         private static List<IPlugin> CreatePlugins(Assembly assembly)
         {
